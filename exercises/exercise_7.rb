83d3937fe94e41f5b4ddc0f0b7455ca004302afa
Chandra <krsna.c@gmail.com>
2017-12-05 21:43:00 +0000

    ex7 complete

--- exercises/exercise_7.rb
+++ exercises/exercise_7.rb
@@ -10,3 +10,8 @@ puts "Exercise 7"
 puts "----------"
 
 # Your code goes here ...
+puts "Name a new store"
+store_from_user = gets.chomp
+
+Store.create(name: store_from_user)
+puts Store.errors.message
--- lib/store.rb
+++ lib/store.rb
@@ -1,5 +1,23 @@
 class Store < ActiveRecord::Base
 
+  has_many :employees
 
+  validates :name,
+    length: { minimum: 3 }
+
+  validates :annual_revenue,
+    numericality: {
+      only_integer: true,
+      greater_than_or_equal_to: 0
+    }
+
+  validate :carries_mens_or_womens
+
+  def carries_mens_or_womens
+    if mens_apparel == false && womens_apparel == false
+      errors.add(:mens_apparel, "either mens_apparel or womens_apparel must be true")
+      errors.add(:womens_apparel, "either mens_apparel or womens_apparel must be true")
+    end
+  end
 
 end
